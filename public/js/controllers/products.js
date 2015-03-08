ddefb939c2b510834cd3f49783ea97cd861be704
DSRoden <Daniel.S.Roden@gmail.com>
2015-03-08 04:03:52 -0500

    bookmarking

--- public/js/controllers/products.js
+++ public/js/controllers/products.js
@@ -2,10 +2,11 @@
   'use strict';
 
   angular.module('product-browser')
-  .controller('ProductsCtrl', ['$scope', '$timeout', '$location', '$state', 'Product', function($scope, $timeout, $location, $state, Product){
+  .controller('ProductsCtrl', ['$scope', '$timeout', '$location', '$state', 'Product', '$filter', function($scope, $timeout, $location, $state, Product, $filter){
     var scope = this;
     scope.title = 'products';
     scope.products = [];
+    scope.bookmarks = [];
 
     // INITIALIZE GET PRODUCTS FUNCTION
     scope.getProductInfo = function(){
@@ -20,7 +21,28 @@
       window.open(url, '_blank', 'location=yes');
     };
 
+    //SAVING ITEMS WITHOUT DUPLICATES
+    scope.bookmarkItem = function(product){
+      var exists = _.find(scope.bookmarks, function(bookmark){
+        return bookmark.itemId[0] === product.itemId[0];
+      });
+      if(exists){ return;
+      } else {
+        scope.bookmarks.push(product);
+      }
+    };
 
+    //RESET ALL
+    scope.resetFilters = function(){
+      scope.getProductInfo();
+      scope.bookmarkedProducts = false;
+    };
 
+    // //ORDER FUNCTION
+    // scope.order = function(predicate, reverse){
+    //   console.log('trying to create order');
+    //   scope.products = orderBy(scope.products, predicate, reverse);
+    //   console.log(scope.products);
+    // };
   }]);
 })();
